d58cbf633146c8e7b7b09ddc55ac3ce7a2c9cfe5
Jesse <gojsse@gmail.com>
2021-09-30 09:33:40 -0500

    GH Pages fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename='px'>
         <App />
       </Router>
     </Provider>
